904342a1f028e3e0eb185d632660527cb9b1306a
Kosinus <charlelie.petracca@gmail.com>
2014-01-29 11:15:25 +0100

    creation des redirections

--- index.php
+++ index.php
@@ -3,14 +3,7 @@ require_once 'include/engine.php';
 // Si non connecté
 if(!isset($_SESSION['id'])) 
     REDIRECT('login.php');
-HTML_HEADER('Accueil');
-?>
-	<div id="content">
-		
-		<?php require('/var/www/ProjetWeb/agenda/selectable.php'); ?>
-			
-	</div>
-
-<?php 
-HTML_FOOTER();
+else
+	REDIRECT('profil.php');
+	
 ?>
