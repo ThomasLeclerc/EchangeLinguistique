d274ac022c346fd5a7999835bf80ec7879151db6
Thomas <leclercthomas@yahoo.fr>
2014-01-29 10:39:46 +0100

    script sql fonctionnel

--- script.sql
+++ script.sql
@@ -1,4 +1,6 @@
+drop table if exists FICHE;
 drop table if exists UTILISATEUR;
+
 
 
 CREATE TABLE UTILISATEUR(
@@ -10,8 +12,8 @@ CREATE TABLE UTILISATEUR(
 	PRIMARY KEY(idUtilisateur)
 )engine=InnoDB;
 
-CREATE TABLE RENSEIGNEMENTS(
-	idRenseignements int not null auto_increment,
+CREATE TABLE FICHE(
+	idFiche int not null auto_increment,
 	langueMaternelle varchar(50),
 	languePerfectionnement varchar(50),
 	age int,
@@ -23,6 +25,14 @@ CREATE TABLE RENSEIGNEMENTS(
 	niveauLangueSysteme varchar(2) default null,
 	complement varchar(500),
 	idUtilisateur int not null,
-	PRIMARY KEY(idRenseignements),
+	PRIMARY KEY(idFiche),
 	CONSTRAINT fk_utilisateur FOREIGN KEY(idUtilisateur) REFERENCES UTILISATEUR(idUtilisateur)	
 )engine=InnoDB;
+
+INSERT INTO UTILISATEUR values(null, "Leclerc", "Thomas", "tleclerc", sha1("quenelle"));
+INSERT INTO UTILISATEUR values(null, "Petracca", "Charlélie", "cpetracca", sha1("quenelle"));
+
+INSERT INTO FICHE values(null, "Francais", "Italien", 22, "14 rue de Beaupaquier 25240 Mouthe", "0600000000", "leclercthomas@yahoo.fr", "Etudiant", "avancé", null, "je fais du gros son avec mes amis", 1);
+INSERT INTO FICHE values(null, "Francais", "Anglais", 25, "In th street", "0600000000", "charlelie.petracca@gmail.com", "Etudiant", "avancé", null, "j\'aime la guitare, les jeux vidéos et Marc Dorcel", 2);
+
+
