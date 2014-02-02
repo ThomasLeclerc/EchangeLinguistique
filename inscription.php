6e2cd7a2859a9c8a3c4a7a01d7169dd400eac333
Thomas <leclercthomas@yahoo.fr>
2014-02-02 16:06:43 +0100

    div content

--- inscription.php
+++ inscription.php
@@ -1,7 +1,28 @@
 <?php
 require_once 'include/engine.php';
-// Si non connecté
-if(!isset($_SESSION['id'])) 
-    REDIRECT('login.php');
+
+HTML_HEADER('INSCRIPTION');
 	
+?>
+<div id="content">
+	<div id="form_inscription">
+		<h2>Demande pour un tandem linguistique</h2>
+		<form action="" method=post>
+			<table>
+				<tr>
+					<td>Dans quelle langue souhaitez-vous vous perfectionner ?</td>
+					<td>
+						<select name=""
+					</td>
+				</tr><tr>
+					<td></td>
+					<td></td>
+				</tr>
+			</table>
+		</form>
+	</div>
+</div>
+
+<?php
+HTML_FOOTER();
 ?>
