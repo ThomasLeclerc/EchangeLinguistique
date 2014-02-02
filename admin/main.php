016ab35995df41b138ca2e67894f472d07b5c1f8
Kosinus <charlelie.petracca@gmail.com>
2014-02-02 18:25:07 +0100

    tableau

--- admin/main.php
+++ admin/main.php
@@ -9,6 +9,41 @@ HTML_HEADER('Administration');
 ?>
 <!--liste des menus admin-->
 <div>
+
+<table id="FichesTable">
+    <thead>
+        <tr>
+            <td>Langue maternelle</td>
+            <td>Age</td>
+            <td>Sexe</td>
+            <td>Ville</td>
+            <td>Profession</td>
+            <td>Langue de perfectionnement</td>
+            <td>Niveau</td>
+            <td>Niveau (système européen)</td>
+        </tr>
+    </thead>
+    <tbody><?php
+        $query=SQL("select * from UTILISATEUR u, FICHE f order by idFiche desc");
+		while($row=$query->fetch_object())
+		{
+			echo '<tr style="background-color: lightgreen;">';
+			echo '<td>'.$row->langueMaternelle.'</td>';
+			echo '<td>'.$row->age.'</td>';
+			if($row->civilite=='M.')
+				echo '<td>M</td>';
+			else
+				echo '<td>F</td>';
+			echo '<td>'.$row->ville.'</td>';
+			echo '<td>'.$row->profession.'</td>';
+			echo '<td>'.$row->languePerfectionnement.'</td>';
+			echo '<td>'.$row->niveauLanguePerfectionnement.'</td>';
+			echo '<td>'.$row->niveauLangueSysteme.'</td>';
+			echo '</tr>';
+		}
+    ?></tbody>
+</table>
+
 </div>
 <?php 
 HTML_FOOTER();
