a54ad5500a7ba52e65cd1f6e61d91bdeec6cf6c0
Thomas <leclercthomas@yahoo.fr>
2014-02-04 00:47:00 +0100

    modifs inscription

--- inscription.php
+++ inscription.php
@@ -9,57 +9,98 @@ if(isset($_POST["valide"])){
 	}else{
 		$complement = $_POST["complement"];
 	}
-		
 	$result = SQL('INSERT INTO FICHE values(
-				null, "'.$_POST["nom"].'",	"'.$_POST["prenom"].'",	'.$_POST["langueMaternelle"].', '.$_POST["languePerfectionnement"].', '.$_POST["age"].', "'.$_POST["sexe"].'", "'.$_POST["adresse"].'", "'.$_POST["codePostal"].'", "'.strtoupper($_POST["ville"]).'", 		"'.$_POST["tel"].'", "'.$_POST["mail"].'", "'.$_POST["profession"].'", "'.$_POST["niveauLanguePerfectionnement"].'", "'.$_POST["niveauLangueSysteme"].'", "'.$complement.'")');
+						null, 
+						"'.$_POST["nom"].'",	
+						"'.$_POST["prenom"].'",	
+						'.$_POST["langueMaternelle"].', 
+						'.$_POST["languePerfectionnement"].', 
+						'.$_POST["age"].', 
+						"'.$_POST["sexe"].'", 
+						"'.$_POST["adresse"].'", 
+						"'.$_POST["codePostal"].'", 
+						"'.strtoupper($_POST["ville"]).'", 		
+						"'.$_POST["tel"].'", 
+						"'.$_POST["mail"].'", 
+						"'.$_POST["profession"].'", 
+						"'.$_POST["niveauLanguePerfectionnement"].'", 
+						"'.$_POST["niveauLangueSysteme"].'", 
+						"'.$complement.'")');
 				
-
 	if($result==true){
 		echo "<div class='msg_0'>Votre inscription au tandem linguistique a bien été prise en compte. Vous serez informés par email lorsqu'un partenariat sera disponible.</div>";
 	}else{
 		echo "<div class='msg_1'>Un problème est survenu lors de votre inscritpion. veuillez réessayer.</div>";
 	}
-}else{	
+}else{
+	$result=SQL("SELECT * FROM LANGUE");
+	$htmlSelectLangue = '';
+	while($resultat=$result->fetch_object()){
+		$htmlSelectLangue .= "<option value='".$resultat->idLangue."'>".$resultat->libelleLangue;
+	}
+
+
+
+
 ?>
-<script>
-	$("#form_inscription").validate();
+<script>	
+$("#form_inscription").validate();	
+$(document).ready(function(){
+	
+	
+	$("#ajouterLanguePerf").click(function () {
+		var prevHtml = $("#TdLanguePerfectionnement").html();
+		prevHtml =  prevHtml+"<select name='languePerfectionnement' required><?=$htmlSelectLangue?></select>";
+		$("#TdLanguePerfectionnement").html(prevHtml);
+	});
+});
 </script>
 
-	
+
 <div id="FormulaireInscr">
 	<h2>Demande pour un tandem linguistique</h2>
 	<form action="" method=post id="form_inscription">
 	<small>* champs obligatoires</small>
 	<fieldset>
+	<legend>Langues</legend>
 		<table>
 			<tr>
-				<td>Dans quelle langue souhaitez-vous vous perfectionner ? </td>
-				<td>
-					<select name="languePerfectionnement"  required>
-						<option value="0"> - 
-						<option value="1">Français
-						<option value="2">Anglais
-						<option value="3">Allemand
-						<option value="4">Espagnol
-						<option value="5">Russe
-						<option value="6">Chinois
-						<option value="7">Breton
-						<option value="8">Italien
-					</select>*
+				<td>La langue que vous souhaitez perfectionner ? </td>
+				<td id="TdLanguePerfectionnement">
+					<select name="languePerfectionnement" required>
+						<?=$htmlSelectLangue?>
+					</select>
+					<input type="button" id="ajouterLanguePerf" value="ajouter une langue"/>
+					
 				</td>
 			</tr><tr>
-				<td>Quelle est votre langue maternelle ou la langue que vous parlez couramment ?</td>
+				<td  >Votre niveau dans cette langue : </td>
 				<td>
+					<input type="radio" name="niveauLanguePerfectionnement" id="debutant" value="faible"required/><label for="debutant">débutant</label><br/>	
+					<input type="radio" name="niveauLanguePerfectionnement" id="inter" value="intermédiaire" required/><label for="inter">intermédiaire</label>  * <br/>
+					<input type="radio" name="niveauLanguePerfectionnement" id="avance" value="avancé" required/><label for="avance">avancé</label>
+				</td>
+			</tr>	<tr>
+				<td  >Si vous le connaissez, votre niveau dans le système européen : <br/>
+				<a href="Ressources/Descripteur.pdf" target="_blank">(Système européen)</a>
+				</td>
+				<td>
+					<input type="radio" name="niveauLangueSysteme" id="a1" value="A1" /><label for="a1">A1</label>
+					<input type="radio" name="niveauLangueSysteme" id="b1" value="B1" /><label for="b1">B1</label>
+					<input type="radio" name="niveauLangueSysteme" id="c1" value="C1" /><label for="c1">C1</label>
+					  * <br/>	
+					<input type="radio" name="niveauLangueSysteme" id="a2" value="A2" /><label for="a2">A2</label>
+					<input type="radio" name="niveauLangueSysteme" id="b2" value="B2" /><label for="b2">B2</label>
+					<input type="radio" name="niveauLangueSysteme" id="c2" value="C3" /><label for="c2">C2</label><br/>
+					<input type="radio" name="niveauLangueSysteme" id="no" value="" checked /><label for="no">je ne sais pas</label>
+				</td>
+			</tr>
+			<tr><td colspan="2"><hr></td></tr>
+			<tr>
+				<td>Quelle est votre langue maternelle ou <br/>la langue que vous parlez couramment ?</td>
+				<td id="TdLangueMaternelle">
 					<select name="langueMaternelle" required>
-						<option value="0"> - 
-						<option value="1">Français
-						<option value="2">Anglais
-						<option value="3">Allemand
-						<option value="4">Espagnol
-						<option value="5">Russe
-						<option value="6">Chinois
-						<option value="7">Breton
-						<option value="8">Italien
+						<?=$htmlSelectLangue?>
 					</select>*
 				</td>
 			</tr>
@@ -112,31 +153,9 @@ if(isset($_POST["valide"])){
 			<tr>
 				<td><label for="profession">Quelles études poursuivez-vous / <br/>quelle est votre profession ? : </label></td>
 				<td><input type="text" name="profession" id="profession"  maxsize=250 size=30 required/> * </td>
-			</tr><tr>
-				<td  >Comment évaluez-vous votre niveau dans<br/> la langue que vous souhaitez perfectionner : </td>
-				<td>
-					<input type="radio" name="niveauLanguePerfectionnement" id="debutant" value="faible"required/><label for="debutant">débutant</label><br/>	
-					<input type="radio" name="niveauLanguePerfectionnement" id="inter" value="intermédiaire" required/><label for="inter">intermédiaire</label>  * <br/>
-					<input type="radio" name="niveauLanguePerfectionnement" id="avance" value="avancé" required/><label for="avance">avancé</label>
-				</td>
-			</tr><tr>
-				<td  >Si vous le connaissez indiquez votre niveau<br/> de langue dans le système européen : <br/>
-				<a href="Ressources/Descripteur.pdf" target="_blank">(Système européen)</a>
-				</td>
-				<td>
-					<input type="radio" name="niveauLangueSysteme" id="a1" value="A1" /><label for="a1">A1</label>
-					<input type="radio" name="niveauLangueSysteme" id="b1" value="B1" /><label for="b1">B1</label>
-					<input type="radio" name="niveauLangueSysteme" id="c1" value="C1" /><label for="c1">C1</label>
-					  * <br/>	
-					<input type="radio" name="niveauLangueSysteme" id="a2" value="A2" /><label for="a2">A2</label>
-					<input type="radio" name="niveauLangueSysteme" id="b2" value="B2" /><label for="b2">B2</label>
-					<input type="radio" name="niveauLangueSysteme" id="c2" value="C3" /><label for="c2">C2</label><br/>
-					<input type="radio" name="niveauLangueSysteme" id="no" value="" checked /><label for="no">je ne sais pas</label>
-				</td>
-			</tr>	
+			</tr>
 		</table>
-		</fieldset><fieldset>
-		<legend><h4>Plus d'informations ...</h4></legend>
+		<br/><br/>
 		<table>
 			<tr>
 				<td>
