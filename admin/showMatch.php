697756c012f42638c7ec7d6123a2a707c0448402
Kosinus <charlelie.petracca@gmail.com>
2014-02-04 22:34:12 +0100

    matchs

--- admin/showMatch.php
+++ admin/showMatch.php
@@ -2,20 +2,30 @@
     require_once("../include/engine.php");
 	$idFiche1 = $_GET["id"];
 
-	$queryLangue = SQL("select * from FICHE where idFiche=".$idFiche1);
-	$rowLangue=$queryLangue->fetch_object();
-	$queryNbMatch = SQL("	select * 
-							from FICHE 
-							where idLangueMaternelle=".$rowLangue->idLanguePerfectionnement."
-							and idLanguePerfectionnement=".$rowLangue->idLangueMaternelle);
-	$i = 0;
-	while($rowNbMatch=$queryNbMatch->fetch_object())
-	{
-		$i++;
-		$idFiche=$rowNbMatch->idFiche;
-		require("../admin/showFiche.php");//?id=".$rowNbMatch->idFiche);
-		echo '</div>';
-	}
-	echo '<legend>'.$i.' personne(s) correspond(ent)</legend>';
+	$nbMatch=0;
+
+	$queryMatch=SQL("	select f.idFiche
+						from FICHE f, PARLE pa, PERFECTIONNE pe
+						where pe.idLangue in (
+							select idLangue
+							from PARLE where idFiche=".$idFiche1."
+							)
+						and f.idFiche=pe.idFiche
+						and pa.idLangue in (
+							select idLangue from PERFECTIONNE
+							where idFiche=".$idFiche1."
+							)
+						and f.idFiche=pa.idFiche						
+					");
+		while($rowMatch=$queryMatch->fetch_object())
+		{
+			$nbMatch++;
+			$idFiche=$rowMatch->idFiche;
+			echo '<div>';
+			require("../admin/showFiche.php");
+			echo '</div>';
+		}
+
+	echo '<legend><h5>'.$nbMatch.' personne(s) correspond(ent)</h5></legend>';
 
 ?>
