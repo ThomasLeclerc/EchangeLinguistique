a02002711f3437d61087238959617336e573ef8b
Kosinus <charlelie.petracca@gmail.com>
2014-02-06 20:09:16 +0100

    affichage des correspondance 1 par 1

--- admin/showMatch.php
+++ admin/showMatch.php
@@ -1,8 +1,10 @@
 <?php
     require_once("../include/engine.php");
+
 	$idFiche1 = $_GET["id"];
 
 	$nbMatch=0;
+	$fs=0;
 
 	$queryMatch=SQL("	select f.idFiche
 						from FICHE f, PARLE pa, PERFECTIONNE pe
@@ -17,15 +19,63 @@
 							)
 						and f.idFiche=pa.idFiche						
 					");
-		while($rowMatch=$queryMatch->fetch_object())
+	/* ****
+		Requete pour le nombre de correspondances trouvees
+													****** */
+	$queryNbMatch=SQL("	select count(*) as nb
+						from FICHE f, PARLE pa, PERFECTIONNE pe
+						where pe.idLangue in (
+							select idLangue
+							from PARLE where idFiche=".$idFiche1."
+							)
+						and f.idFiche=pe.idFiche
+						and pa.idLangue in (
+							select idLangue from PERFECTIONNE
+							where idFiche=".$idFiche1."
+							)
+						and f.idFiche=pa.idFiche						
+					");
+	$rowNbMatch=$queryNbMatch->fetch_object();
+	$nbMatch=$rowNbMatch->nb;
+	// on affiche les fleches de navigations uniquement s'il y a plusieurs matchs trouves
+	if($nbMatch>1)
+	{
+		echo '<table id="navigationMatch"><tr>';
+		echo '<td><input id="butAv" type="button" value="<" style="width:25px;"/></td>';
+		echo '<td><div id="numMatch" style="width:25px;">1</div></td>';
+		echo '<td><input id="butAp" type="button" value=">" style="width:25px;"/></td>';
+		echo '</tr></table>';
+	}
+	
+	//pour chaque match trouve
+	$i=0;
+	while($rowMatch=$queryMatch->fetch_object())
+	{
+		$i++;
+		$idFiche=$rowMatch->idFiche;
+		
+
+		//au 1er resultat on ouvre un fieldset
+		if($fs==0)
 		{
-			$nbMatch++;
-			$idFiche=$rowMatch->idFiche;
-			echo '<div>';
-			require("../admin/showFiche.php");
-			echo '</div>';
+			if($nbMatch==1)
+				echo '<fieldset><legend><h5>'.$nbMatch.' fiche correspond</h5></legend>';
+			else
+				echo '<fieldset><legend><h5>'.$nbMatch.' fiches correspondent</h5></legend>';
+			$fs=1;
 		}
 
-	echo '<legend><h5>'.$nbMatch.' personne(s) correspond(ent)</h5></legend>';
+		if($i>1)
+			echo '<div id="match"'.$idFiche.' class=match'.$i.' style="display:none;">';
+		else
+			echo '<div id="match"'.$idFiche.' class=match'.$i.'>';
+			
+		require("../admin/showFiche.php");
+		echo '</div>';
+	}
+	//si un fieldset a ete ouvert, on le ferme
+	if($fs==1)
+		echo '</fieldset>';
+	echo '<div id="nbMatch" style="visibility:hidden;">'.$nbMatch.'</div>';
 
 ?>
