218a37fd8fb06481a6f1643aa375bdbf1bc670af
Thomas <leclercthomas@yahoo.fr>
2014-02-15 18:25:34 +0100

    style gestion

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=ISO-8859-1"> 
 		<meta name="description" content=""/>
 		<title><?=$titre?> - Dynamic Web Project - </title>
 		<link rel="stylesheet" type="text/css" href="<?=SHORT_RACINE?>styles/style.css" />
