d5a524c415b087717be2364b8343985999237462
Kosinus <charlelie.petracca@gmail.com>
2014-02-18 13:07:01 +0100

    fichier pour changer de mdp

---
+++ admin/profil.php
@@ -0,0 +1,8 @@
+<?php
+// Moteur + vérif des droits
+require_once '../include/engine.php';
+// Si non connecté
+if(!isset($_SESSION['id'])) 
+    REDIRECT('../login.php');
+// Entete
+HTML_HEADER('Tandems');
