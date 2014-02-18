eea732ddf7cb6ae6c4a77c331973f2ee481003fc
Kosinus <charlelie.petracca@gmail.com>
2014-02-18 14:04:32 +0100

    modif infos et/ou mdp

--- admin/profil.php
+++ admin/profil.php
@@ -6,9 +6,25 @@ if(!isset($_SESSION['id']))
     REDIRECT('../login.php');
 // Entete
 HTML_HEADER('Profil');
+
+if(isset($_GET['error']))
+	echo '<fieldset>Erreur lors de la modification du mot de passe!<br /> Modification annulée!</fieldset>';
 ?>
 
 <form id="profileForm" method="POST" action="profileRequest.php">
+
+<?php
+$query=SQL("select * from UTILISATEUR where idUtilisateur=".$_SESSION['id']);
+$row=$query->fetch_object();
+echo '<table>';
+echo '<tr><td><label>Nom : </label></td><td><input type="text" name="nomProfil" value="'.$row->nomUtilisateur.'" /></td></tr>';
+echo '<tr><td><label>Prenom : </label></td><td><input type="text" name="prenomProfil" value="'.$row->prenomUtilisateur.'" /></td></tr>';
+echo '<tr><td><label>Login : </label></td><td><input type="text" name="loginProfil" value="'.$row->loginUtilisateur.'" /></td></tr>';
+echo '<tr><td><label>Mot de passe : </label></td><td><input type="password" name="mdpProfil" /></td></tr>';
+echo '<tr><td><label>Nouveau mot de passe : </label></td><td><input type="password" name="newMdpProfil" /></td></tr>';
+echo '<tr><td><label>Confirmation nouveau mdp : </label></td><td><input type="password" name="newMdpProfil2" /></td></tr>';
+echo '<tr><td><input type="submit" value="Modifier"/></td></tr>';
+?>
 
 </form>
 
--- admin/profileRequest.php
+++ admin/profileRequest.php
@@ -5,8 +5,54 @@ require_once '../include/engine.php';
 if(!isset($_SESSION['id'])) 
     REDIRECT('../login.php');
 
-
-
+$idUtilisateur=$_SESSION['id'];
+// Récup des données du formulaire
+$nomUtilisateur=$_POST['nomProfil'];
+$prenomUtilisateur=$_POST['prenomProfil'];
+$loginUtilisateur=$_POST['loginProfil'];
+$mdpUtilisateur=$_POST['mdpProfil'];
+$mdpUtilisateur1=$_POST['newMdpProfil'];
+$mdpUtilisateur2=$_POST['newMdpProfil2'];
+// Si changement de mdp
+if($mdpUtilisateur1!="")
+{
+	$queryMdp=SQL("select password from UTILISATEUR where idUtilisateur=".$idUtilisateur);
+	$rowMdp=$queryMdp->fetch_object();
+	$mdpBase=$rowMdp->password;
+	// On vérifie que les deux mdp taper 
+	// sont les mêmes(au cas ou javascript desactivé)
+	if($mdpUtilisateur1==$mdpUtilisateur2 && hash("sha1",$mdpUtilisateur)==$mdpBase)
+		// Requete de la mise à jour
+		$query_modif=SQL
+		("  
+			UPDATE UTILISATEUR 
+			SET nomUtilisateur='".$nomUtilisateur."',
+				prenomUtilisateur='".$prenomUtilisateur."',
+				loginUtilisateur='".$loginUtilisateur."',
+				password='".hash("sha1",$mdpUtilisateur1)."'
+			WHERE idUtilisateur=".$idUtilisateur
+		);
+	// Si les mdp sont differents
+	else
+	{
+		// Redirection
+		REDIRECT('profil.php?error=1');
+	}
+}      
+// Si pas de changement de mdp
+else
+{
+	// Requete de la mise à jour
+	$query_modif=SQL
+	("
+		UPDATE UTILISATEUR
+		SET nomUtilisateur='".$nomUtilisateur."',
+			prenomUtilisateur='".$prenomUtilisateur."',
+			loginUtilisateur='".$loginUtilisateur."'
+		WHERE idUtilisateur=".$idUtilisateur
+	);
+}
+REDIRECT('Profil.php');
 
 
 
