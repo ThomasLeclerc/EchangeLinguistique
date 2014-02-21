153e83548e8759f08255c44889927292b768db59
Kosinus <charlelie.petracca@gmail.com>
2014-02-21 14:05:39 +0100

    creation fichier gestion users

---
+++ admin/user.php
@@ -0,0 +1,18 @@
+<?php
+// Moteur + vérif des droits
+require_once '../include/engine.php';
+// Si non connecté
+if(!isset($_SESSION['id'])) 
+    REDIRECT('../login.php');
+// Entete
+HTML_HEADER('Gestion des utilisateurs');
+?>
+
+
+<?php
+
+
+
+
+HTML_FOOTER();
+?>
