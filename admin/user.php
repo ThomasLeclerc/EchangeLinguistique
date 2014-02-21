aeb6d0789fa74f1be5d74e8477248d9424fb3167
Kosinus <charlelie.petracca@gmail.com>
2014-02-21 15:44:50 +0100

    suppression user

--- admin/user.php
+++ admin/user.php
@@ -10,13 +10,13 @@ HTML_HEADER('Ajouter un utilisateur');
 
 <form id="userForm" method="POST" action="userRequest.php">
 <table>
-<tr><td><label>Nom : </label></td><td><input type="text" name="nomUser" /></td></tr>
-<tr><td><label>Prenom : </label></td><td><input type="text" name="prenomUser" /></td></tr>
-<tr><td><label>Login : </label></td><td><input type="text" name="loginUser" /></td></tr>
-<tr><td><label>E-mail : </label></td><td><input type="text" name="emailUser" /></td></tr>
+<tr><td><label>Nom : </label></td><td><input type="text" name="nomUser" required/></td></tr>
+<tr><td><label>Prenom : </label></td><td><input type="text" name="prenomUser" required/></td></tr>
+<tr><td><label>Login : </label></td><td><input type="text" name="loginUser" required/></td></tr>
+<tr><td><label>E-mail : </label></td><td><input type="text" name="emailUser" required/></td></tr>
 <tr><td><label>Recoit emails du site : </label></td><td><input type="checkbox" name="recoitEmail" /></td></tr>
-<tr><td><label>Mot de passe : </label></td><td><input type="password" name="pass1User" /></td></tr>
-<tr><td><label>Confirmer mdp : </label></td><td><input type="password" name="pass2User" /></td></tr>
+<tr><td><label>Mot de passe : </label></td><td><input type="password" name="pass1User" required/></td></tr>
+<tr><td><label>Confirmer mdp : </label></td><td><input type="password" name="pass2User" required/></td></tr>
 </table>
 <input type="submit" value="Ajouter" />
 </form>
