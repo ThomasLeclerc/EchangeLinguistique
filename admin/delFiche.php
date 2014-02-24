e77daf5c6206cd1842a82cc28ec54b05819b8947
Thomas <leclercthomas@yahoo.fr>
2014-02-24 14:08:38 +0100

    mail suppression fiche

--- admin/delFiche.php
+++ admin/delFiche.php
@@ -3,10 +3,20 @@
 	// Si non connecté
 	if(!isset($_SESSION['id'])) 
 		REDIRECT('../login.php');
-	$idFiche = $_POST["num"];
+		
+	$idFiche = 13;//$_POST["num"];
 	$resultEmail = ('SELECT mail from FICHE where idFiche='.$idFiche);
-	$mail = $resultEmail->fetch_object()->mail;
+	$mail = ($resultEmail->fetch_object())->mail;
 	
 	
 	$query=SQL("delete from FICHE where idFiche=".$idFiche);
+	if($query){
+		$sujet = 'Tandems Linguistique : Suppression de votre inscription';
+		$msg = 'Bonjour, <br/>
+		\t Nous tenons à vous informer que votre inscription au programme d\'échanges linguistiques a été supprimée.
+		Si vous le souhaitez vous pouvez vous ré-inscrire sur le site.
+		Cordialement,<br/>
+		'.$_SESSION['nom'];
+		sendEmail($_SESSION['email'], $mail, $sujet, $msg);
+	}
 ?>
